1a14d5d3aac34816105741c1e181abd69d09c957
phil51297 <b.philippe.97@gmail.com>
2025-02-13 09:51:54 +0100

    feat: expose header with module federation

--- header/rsbuild.config.ts
+++ header/rsbuild.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(),
+  pluginModuleFederation({
+      name: 'header',
+      exposes: {
+        './header': './src/header.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
 });
