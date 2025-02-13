40ae9727e1797444af5e7f446eb3220d35ad5e7a
phil51297 <b.philippe.97@gmail.com>
2025-02-13 09:46:45 +0100

    feat: added the producer header

---
+++ header/src/header.tsx
@@ -0,0 +1,3 @@
+const Header = () => <header><h1>Header of Micro Frontend</h1></header>;
+
+export default Header;
--- header/src/index.tsx
+++ header/src/index.tsx
@@ -1,13 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import('./bootstrap');
 
-const rootEl = document.getElementById('root');
-if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-}
